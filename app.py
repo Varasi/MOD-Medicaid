82716715792af5ee7d8cc413aa36271bec1e8d55
Sai Teja <sai.boddana@varasi.com>
2024-11-29 17:00:33 +0530

    changes required for deployment

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ HealthConnectorCdkStack(app, "HealthConnectorCdkStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    env=cdk.Environment(account='891377257073', region='us-west-1'),
+    env=cdk.Environment(account='443370714691', region='ap-south-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
